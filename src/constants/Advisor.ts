9ac948b016aa838b2d0e0778e3968b90cc6b5766
nicktnc24 <nicktnc24@gmail.com>
2025-11-20 20:02:21 -0600

    updated pics

--- src/constants/Advisor.ts
+++ src/constants/Advisor.ts
@@ -3,14 +3,14 @@ import type {AdvisorList} from "./constants";
 export const Advisors: AdvisorList = {
     pauline: {
         image: {
-            src: "/headshots/advisor.jpg",
+            src: "/headshots/advisor.png",
             width: 256,
             height: 256,
             alt: "Pauline Wade Headshot",
         },
         name: "Pauline Wade",
-        major: "Compuer Science and Engineeering",
-        role: "Associate Professor of Practice",
+        major: "Computer Science and Engineering",
+        role: "Prof. of Practice",
         link: "https://engineering.tamu.edu/cse/profiles/wade-pauline.html",
     },
 }
